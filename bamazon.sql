33425a9527d06844c4f4e530403ff93fe894bb27
victoria field <victoria.shadid@gmail.com>
2017-09-14 15:16:48 -0500

    added photos

--- bamazon.sql
+++ bamazon.sql
@@ -3,40 +3,33 @@ CREATE DATABASE bamazon;
 
 USE bamazon;
 
+DROP TABLE IF EXISTS products;
+
 CREATE TABLE products(
-  id INT NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   item_name VARCHAR(100) NOT NULL,
   department_name VARCHAR(145) NOT NULL,
   price INT(11),
-  stock_quanity INT(11),
-  PRIMARY KEY (id)
+  stock_quanity INT(11)
 );
-INSERT INTO product(id, name_product, department_name, price, stock_qty)
-VALUES(01, "bananas", "grocery", 5, 100);
 
-INSERT INTO product(id, name_product, department_name, price, stock_qty)
-VALUES(02, "banana butter", "grocery", 2,  10);
+INSERT INTO products (item_name, department_name, price, stock_quantity)
+VALUES("bananas", "grocery", 5, 100);
 
-INSERT INTO product(id, name_product, department_name, price, stock_qty)
-VALUES(03, "banana yellow rug", "household", 50, 16);
+("banana butter", "grocery", 2,  10);
 
-INSERT INTO product(id, name_product, department_name, price, stock_qty)
-VALUES(04, "banana paste", "grocery", 3, 90);
+("banana yellow rug", "household", 50, 16);
 
-INSERT INTO product(id, name_product, department_name, price, stock_qty)
-VALUES(05, "banana icecream", "grocery", 6, 100);
+("banana paste", "grocery", 3, 90);
 
-INSERT INTO product(id, name_product, department_name, price, stock_qty)
-VALUES(06, "banana tree", "outside", 30, 25);
+("banana icecream", "grocery", 6, 100);
 
-INSERT INTO product(id, name_product, department_name, price, stock_qty)
-VALUES(07, "bananaOgrams", "household", 9, 15);
+("banana tree", "outside", 30, 25);
 
-INSERT INTO product(id, name_product, department_name, price, stock_qty)
-VALUES(08, "towel with banana", "household", 12, 30);
+("bananaOgrams", "household", 9, 15);
 
-INSERT INTO product(id, name_product, department_name, price, stock_qty)
-VALUES(09, "banana art", "household", 50, 5);
+("towel with banana", "household", 12, 30);
 
-INSERT INTO product(id, name_product, department_name, price, stock_qty)
-VALUES(01, "banana lamps", "household", 40, 26);
+("banana art", "household", 50, 5);
+
+("banana lamps", "household", 40, 26);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,26 +6,19 @@ var Table = require('cli-table');
 var purchaseId = [];
 
 // create the connection information for the sql database
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
+var connection = mysql.createConnection(keys.db);
 
-  // Your username
-  user: "root",
 
-  // Your password
-  password: keys.db,
-  database: "bamazon"
+connection.connect(function(err) {
+  console.log(err)
 });
-
 
 productTable();
 
 function productTable(){
-  connection.connect();
+
   connection.query("SELECT * FROM products", function(error, results){
-    if(error) throw error;
-    console.log(error);
+    if ( error ) return console.log(error);
 
     var table = new Table({
       head:['Id', 'Product Name', 'department', 'Item Price', 'Stock Quanity',]
@@ -34,41 +27,53 @@ function productTable(){
     for (i=0; i < results.length; i++){
       var tempArray = [];
       for(var key in results[i]){
-        tempArray.push(ressults[i][key]);
+        tempArray.push(results[i][key]);
       }
 
       table.push(tempArray);
     }
 
     console.log(table.toString());
+    askQuestions();
   });
 
-  askQuestions();
 }
 
 function askQuestions(){
-  inquirer.prompt([{
-
-    type: 'input',
-    name: 'qa1',
-    message: 'What would you like to purchase?'
-  }]).then(function(answers){
-    console.log(answers.qa1);
-
-    var answer1 = answers.qa1;
-
-    inquirer.prompt([{
-
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'qa1',
+      message: 'What would you like to purchase?'
+    },
+    {
       type: "input",
       name: 'q2',
       message: 'How many would you like?'
+    }
+  ]).then(function(answers){
+    // console.log(answers.qa1);
 
-    }]).then(function(answers){
-      console.log(answers.q2);
+    // console.log(answers.q2);
+    connection.query('SELECT * FROM products WHERE id = ' + answers.qa1, function(err, result) {
+      var new_amount = parseInt(result[0].stock_quanity - answers.q2);
+      console.log(new_amount);
+      console.log(result);
 
-      connection.query("SELECT * FROM products WHERE id= " + answer1, function(error, results){
-        console.log(results);
-      });
-    })
+      if ( new_amount > 0 ) {
+        connection.query("UPDATE products SET stock_quanity = ? WHERE id = ?", [ new_amount, answers.qa1 ],
+          function(error, results) {
+            if ( error ) return console.log(error);
+              var totalCost = (result[0].price * answers.q2)
+              console.log("you owe "+ totalCost);
+            productTable();
+        });
+      } else {
+        console.log('Slow your roll...');
+        productTable();
+      }
+    });
+
+
   });
 }
